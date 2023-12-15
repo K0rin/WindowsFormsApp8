8c8e1b27eba513dda2bf3180431ac31e45462d2e
K0rin <34002181+K0rin@users.noreply.github.com>
2023-12-15 19:47:03 +0200

    Add project files.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.seeWrongAnswers = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startTest
+            // 
+            this.startTest.Location = new System.Drawing.Point(305, 47);
+            this.startTest.Name = "startTest";
+            this.startTest.Size = new System.Drawing.Size(175, 35);
+            this.startTest.TabIndex = 0;
+            this.startTest.Text = "Start a test";
+            this.startTest.UseVisualStyleBackColor = true;
+            this.startTest.Click += new System.EventHandler(this.startTest_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of correct answers";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of wrong answers";
+            // 
+            // seeWrongAnswers
+            // 
+            this.seeWrongAnswers.Location = new System.Drawing.Point(305, 356);
+            this.seeWrongAnswers.Name = "seeWrongAnswers";
+            this.seeWrongAnswers.Size = new System.Drawing.Size(175, 36);
+            this.seeWrongAnswers.TabIndex = 3;
+            this.seeWrongAnswers.Text = "see wrong answers";
+            this.seeWrongAnswers.UseVisualStyleBackColor = true;
+            this.seeWrongAnswers.Click += new System.EventHandler(this.seeWrongAnswers_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Status of exam: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.seeWrongAnswers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button seeWrongAnswers;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+
+        public string sText;
+        string[] errorAnswers = new string[30];
+        int errorFlag = 0;
+        char[] correctAnswers = { 'B', 'D', 'A', 'A', 'C', 'A', 'B', 'A', 'C', 'D', 'B', 'C', 'D', 'A', 'D', 'C', 'C', 'B', 'D', 'A' };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void startTest_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            char[] userAnswers;
+            int errors = 0;
+            errorFlag = 0;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                StreamReader f = new StreamReader(filePath);
+                int answerCounter = 0;
+                
+                
+                while (!f.EndOfStream)
+                {
+                    sText = f.ReadLine();
+                    userAnswers = sText.ToCharArray();
+                    MessageBox.Show(sText);
+                    for (int i = 0; i < userAnswers.Length; i++) 
+                    {
+                        answerCounter++;
+                        if (userAnswers[i] != correctAnswers[i])
+                        {
+                            errors++;
+                            errorFlag = 1;
+                            string wrongAnswer = answerCounter.ToString()+"-"+userAnswers[i].ToString();
+                            errorAnswers[i] = wrongAnswer;
+                        }
+                    }
+                }
+                f.Close();
+            }
+
+            label1.Text = "Total number of correctly answered questions: " + (correctAnswers.Length - errors).ToString();
+            label2.Text = "Total number of wrong answered questions: " + errors.ToString();
+            if (errors < 5)
+            {
+                label3.Text = label3.Text + " Passed";
+            }
+            else 
+            {
+                label3.Text = label3.Text + " Failed";
+            } 
+        }
+
+        private void seeWrongAnswers_Click(object sender, EventArgs e)
+        {
+            if (errorFlag == 0)
+            {
+                return;
+            }
+            else 
+            {
+                string betaS = "";
+                for (int i = 0; i < errorAnswers.Length; i++)
+                {
+                    if (errorAnswers[i] == null)
+                    {
+                        continue;
+                    }
+                    else{
+                        string a = errorAnswers[i] + "\n";
+                        betaS = betaS + a;
+                    }                 
+                }
+                MessageBox.Show(betaS);
+            }
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
